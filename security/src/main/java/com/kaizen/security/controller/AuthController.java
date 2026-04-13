ff7598f892c76be7344305a761ed20c5a98ff22d
KAIZENCODER <kpidibadavid1@gmail.com>
2026-04-13 16:33:21 +0000

    CHECK UPDATE AUTH

--- security/src/main/java/com/kaizen/security/controller/AuthController.java
+++ security/src/main/java/com/kaizen/security/controller/AuthController.java
@@ -4,18 +4,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping; 
 
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/v1/auth")
 public class AuthController {   
-    
+ 
     @GetMapping("test")
     public String getMethodName( ) {
         return "Hello World";
-    }
-    
+    } 
 }
